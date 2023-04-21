eea6ed47cb7f17755e3e8ff4b6cfc916b9d1223f
LimKwangyoung <kokoz22@naver.com>
2023-04-21 17:12:15 +0900

    브루트 포스

---
+++ Level_12/2798.py
@@ -0,0 +1,28 @@
+# Solution 1
+from itertools import combinations
+
+N, M = map(int, input().split())
+num_lst = list(map(int, input().split()))
+
+result = 0
+for i in list(combinations(num_lst, 3)):
+    # sum for each combination.
+    total = sum(i)
+    if result < total <= M:
+        result = total
+print(result)
+
+# Solution 2
+N, M = map(int, input().split())
+num_lst = list(map(int, input().split()))
+
+num_lst.sort(reverse=True)
+result = 0
+for i in range(N - 2):
+    for j in range(i + 1, N - 1):
+        for k in range(j + 1, N):
+            # sum for each case. faster than Solution 1 (using module).
+            total = num_lst[i] + num_lst[j] + num_lst[k]
+            if result < total <= M:
+                result = total
+print(result)
