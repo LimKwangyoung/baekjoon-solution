dcacc02e0d8f0af6543315423b810c91ab06d248
LimKwangyoung <kokoz22@naver.com>
2023-05-09 16:35:04 +0900

    정렬

--- Level_13/11651.py
+++ Level_13/11651.py
@@ -6,7 +6,7 @@ pair_lst = []
 for _ in range(N):
     pair = list(map(int, sys.stdin.readline().split()))
     pair_lst.append(pair)
-pair_lst.sort()
+pair_lst.sort(key=lambda x: (x[1], x[0]))
 
 for i in pair_lst:
     print(i[0], i[1])
