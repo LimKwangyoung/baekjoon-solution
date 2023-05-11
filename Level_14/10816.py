672def850d430eca204a04330df47e45ed480c57
LimKwangyoung <kokoz22@naver.com>
2023-05-11 22:02:43 +0900

    집합과 맵

--- Level_14/10816.py
+++ Level_14/10816.py
@@ -1,5 +1,7 @@
 import sys
+import collections
 
+# Solution 1
 N = int(sys.stdin.readline())
 num_dict = {}
 for i in sys.stdin.readline().split():
@@ -7,6 +9,15 @@ for i in sys.stdin.readline().split():
         num_dict[i] += 1
     else:
         num_dict[i] = 1
+
+M = int(sys.stdin.readline())
+print(' '.join(str(num_dict[i]) if i in num_dict else '0' for i in sys.stdin.readline().split()))
+
+# Solution 2
+N = int(sys.stdin.readline())
+num_dict = collections.defaultdict(int)
+for i in sys.stdin.readline().split():
+    num_dict[i] += 1
 
 M = int(sys.stdin.readline())
 print(' '.join(str(num_dict[i]) if i in num_dict else '0' for i in sys.stdin.readline().split()))
