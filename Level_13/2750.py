144aa0bdb79780d86fc93b53fb397f52ca968a51
kokoz22 <kokoz22@naver.com>
2023-05-13 15:58:51 +0900

    정렬

--- Level_13/2750.py
+++ Level_13/2750.py
@@ -3,29 +3,40 @@ import sys
 N = int(sys.stdin.readline())
 num_lst = list(map(int, sys.stdin.readlines()))
 
+
 # bubble sort
-for i in range(N - 1):
-    for j in range(N - 1 - i):
-        if num_lst[j] > num_lst[j + 1]:
-            num_lst[j], num_lst[j + 1] = num_lst[j + 1], num_lst[j]
+def bubble_sort(lst: list, n: int) -> None:
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
 
 # selection sort
-for i in range(N - 1):
-    min_idx = i
-    for j in range(i + 1, N):
-        if num_lst[j] < num_lst[min_idx]:
-            min_idx = j
-    num_lst[i], num_lst[min_idx] = num_lst[min_idx], num_lst[i]
+def selection_sort(lst: list, n: int) -> None:
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if lst[j] < lst[min_idx]:
+                min_idx = j
+        lst[i], lst[min_idx] = lst[min_idx], lst[i]
+
 
 # insertion sort
-for i in range(1, N):
-    j = i
-    tmp = num_lst[i]
-    while j > 0 and num_lst[j - 1] > tmp:
-        num_lst[j] = num_lst[j - 1]
-        j -= 1
-    num_lst[j] = tmp
+def insertion_sort(lst: list, n: int) -> None:
+    for i in range(1, n):
+        j = i
+        tmp = lst[i]
+        while j > 0 and lst[j - 1] > tmp:
+            lst[j] = lst[j - 1]
+            j -= 1
+        lst[j] = tmp
+
 
 # print elements by ascending.
+bubble_sort(num_lst, N)
+selection_sort(num_lst, N)
+insertion_sort(num_lst, N)
+
 for num in num_lst:
     print(num)
--- Level_13/2751.py
+++ Level_13/2751.py
@@ -6,27 +6,38 @@ num_lst = list(map(int, sys.stdin.readlines()))
 # built-in function
 num_lst.sort()
 
-# heap sort
-
-# merge sort
 
 # quick sort
-def partition(lst: list) -> None:
-    n = len(lst)
-    pl, pr = 0, n - 1
-    x = lst[n // 2]
+def qsort(lst: list, left: int, right: int) -> None:
+    pl, pr = left, right
+    x = lst[(left + right) // 2]
 
-    while pl < pr:
+    while pl <= pr:
         while lst[pl] < x:
             pl += 1
         while x < lst[pr]:
             pr -= 1
-        if pl < pr:
+        if pl <= pr:
             lst[pl], lst[pr] = lst[pr], lst[pl]
             pl += 1
             pr -= 1
 
+    if left < pr:
+        qsort(lst, left, pr)
+    if pl < right:
+        qsort(lst, pl, right)
+
+
+def quick_sort(lst: list, n: int) -> None:
+    qsort(lst, 0, n - 1)
+
+
+# merge sort
+
+# heap sort
 
 # print elements by ascending.
+quick_sort(num_lst, N)
+
 for i in num_lst:
     print(i)
