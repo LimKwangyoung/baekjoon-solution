368b1d8d8c52c6b735edb1e335bcad9328cf58a2
kokoz22 <kokoz22@naver.com>
2023-05-22 20:48:59 +0900

    약수, 배수와 소 2

--- Level_15/1929.py
+++ Level_15/1929.py
@@ -27,10 +27,15 @@ else:
 # Solution 2 (Sieve of Eratosthenes)
 M, N = map(int, sys.stdin.readline().split())
 
-num_lst = [False, False] + [True] * (N - 1)
-for i in range(2, N + 1):
+num_lst = [False, False] + [True] * (N - 1)  # it starts at 0.
+i = 2
+while i * i <= N:
     if num_lst[i]:
         for j in range(2 * i, N + 1, i):
             num_lst[j] = False  # it is faster than Solution 1.
+    if i == 2:
+        i += 1
+    else:  # only handle odd numbers.
+        i += 2
 
 print('\n'.join(str(i) for i in range(M, N + 1) if num_lst[i]))
--- Level_15/4948.py
+++ Level_15/4948.py
@@ -1,13 +1,53 @@
 import sys
 
-num_lst = [False, False] + [True] * (2 * 123456 - 1)
+# Solution 1
+# num_lst = [False, False] + [True] * (2 * 123456 - 1)
+# while True:
+#     n = int(sys.stdin.readline())
+#     if n <= 0:
+#         break
+#
+#     i = 2
+#     while i * i <= 2 * n:
+#         if num_lst[i] is True:
+#             for j in range(2 * i, 2 * n + 1, i):
+#                 num_lst[j] = False
+#         i += 1
+#     print(sum(num_lst[n + 1:2 * n + 1]))
+
+
+# Solution 2
+def prime_list(num: int) -> list:
+    if num <= 1:
+        return []
+    num_lst = [False, False] + [True] * (num - 1)  # it starts at 0.
+    i = 3  # only handle odd numbers.
+    while i * i <= num:
+        if num_lst[i]:
+            for j in range(2 * i, num + 1, i):
+                num_lst[j] = False
+        i += 2
+    return [2] + [i for i in range(3, num + 1, 2) if num_lst[i]]
+
+
+def bin_search(lst: list, key: int) -> int:
+    pl, pr = 0, len(lst) - 1
+
+    while pl <= pr:
+        pc = (pl + pr) // 2
+        if lst[pc] == key:
+            return pc + 1
+        elif lst[pc] < key:
+            pl = pc + 1
+        else:
+            pr = pc - 1
+    return pl
+
+
+Prime_List = prime_list(2 * 123456)
+
 while True:
     n = int(sys.stdin.readline())
     if n <= 0:
         break
-
-    for i in range(2, 2 * n + 1):
-        if num_lst[i] is True:
-            for j in range(2 * i, 2 * n + 1, i):
-                num_lst[j] = False
-    print(sum(num_lst[n + 1:2 * n + 1]))
+    print(bin_search(Prime_List, 2 * n) - bin_search(Prime_List, n))
