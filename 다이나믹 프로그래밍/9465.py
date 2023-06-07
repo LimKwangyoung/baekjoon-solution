edd00e7d6265e2010e536fd2a223f75df15ef55e
kokoz22 <kokoz22@naver.com>
2023-06-08 00:44:21 +0900

    다이나믹 프로그래밍

--- 다이나믹 프로그래밍/9465.py
+++ 다이나믹 프로그래밍/9465.py
@@ -6,9 +6,13 @@ for _ in range(T):
     n = int(sys.stdin.readline())
     num_lst = [list(map(int, sys.stdin.readline().split())), list(map(int, sys.stdin.readline().split()))]
 
-    num_lst[0][1] += num_lst[1][0]
-    num_lst[1][1] += num_lst[0][0]
-    for i in range(2, n):
-        num_lst[0][i] += max(num_lst[1][i - 2], num_lst[1][i - 1])
-        num_lst[1][i] += max(num_lst[0][i - 2], num_lst[0][i - 1])
+    for i in range(n):
+        if i == 0:
+            continue
+        elif i == 1:
+            num_lst[0][i] += num_lst[1][0]
+            num_lst[1][i] += num_lst[0][0]
+        else:
+            num_lst[0][i] += max(num_lst[1][i - 2], num_lst[1][i - 1])
+            num_lst[1][i] += max(num_lst[0][i - 2], num_lst[0][i - 1])
     print(max(num_lst[0][-1], num_lst[1][-1]))
