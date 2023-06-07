fe5b54e6b04662ac3807c68772411083db5ad3d9
kokoz22 <kokoz22@naver.com>
2023-06-07 23:54:27 +0900

    다이나믹 프로그래밍

--- 다이나믹 프로그래밍/9465.py
+++ 다이나믹 프로그래밍/9465.py
@@ -1,9 +1,23 @@
 import sys
 
+
 T = int(sys.stdin.readline())
 
 for _ in range(T):
     n = int(sys.stdin.readline())
-    lst_1 = list(map(int, sys.stdin.readline().split()))
-    lst_2 = list(map(int, sys.stdin.readline().split()))
+    lst = list(map(int, sys.stdin.readline().split())) + list(map(int, sys.stdin.readline().split()))
 
+    score, max_score = 0, -sys.maxsize
+    def bottomup(idx: int) -> int:
+        global score
+        if idx % 10 == (n - 1):
+            return max(score, max_score)
+
+        score += lst[idx]
+        if idx % 10 <= 3:
+            bottomup((idx + 6) % 10)
+            if idx % 10 <= 2:
+                bottomup((idx + 7) % 10)
+        return score
+
+    print(max(bottomup(0), bottomup(n)))
