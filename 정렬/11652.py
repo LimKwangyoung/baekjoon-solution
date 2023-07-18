b95b96944cf6af18f937266e7768e80bdb05f18f
kokoz22 <kokoz22@naver.com>
2023-07-18 15:47:52 +0900

    정렬

--- 정렬/11652.py
+++ 정렬/11652.py
@@ -1,5 +1,11 @@
 import sys
 
 N = int(sys.stdin.readline())
-lst = list(map(int, sys.stdin.readlines()))
+num_lst = [int(sys.stdin.readline()) for _ in range(N)]
 
+num_set = sorted(list(set(num_lst)), reverse=True)
+max_num, max_cnt = num_set[0], num_lst.count(num_set[0])
+for i in num_set[1:]:
+    if num_lst.count(i) > max_cnt:
+        max_num = i
+print(max_num)
