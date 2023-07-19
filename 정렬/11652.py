9acc6d6ea65b7b963ede1c343cc71bcf43d2a2cf
kokoz22 <kokoz22@naver.com>
2023-07-19 14:43:57 +0900

    정렬

--- 정렬/11652.py
+++ 정렬/11652.py
@@ -1,11 +1,27 @@
 import sys
+import collections
 
+# Solution 1
+N = int(sys.stdin.readline())
+
+num_dict = {}
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num in num_dict:
+        num_dict[num] += 1
+    else:
+        num_dict[num] = 1
+
+lst = []
+for i in num_dict:
+    lst.append((i, num_dict[i]))
+lst.sort(key=lambda x: (-x[1], x[0]))
+print(lst[0][0])
+
+# Solution 2
 N = int(sys.stdin.readline())
 num_lst = [int(sys.stdin.readline()) for _ in range(N)]
 
-num_set = sorted(list(set(num_lst)), reverse=True)
-max_num, max_cnt = num_set[0], num_lst.count(num_set[0])
-for i in num_set[1:]:
-    if num_lst.count(i) > max_cnt:
-        max_num = i
-print(max_num)
+lst = collections.Counter(num_lst).most_common()
+lst.sort(key=lambda x: (-x[1], x[0]))
+print(lst[0][0])
