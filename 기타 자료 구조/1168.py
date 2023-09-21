70df3afc92a108c42835da6f30fd4c76f9e141d5
kokoz22 <kokoz22@naver.com>
2023-09-21 19:21:07 +0900

    기타 자료 구조

--- 기타 자료 구조/1168.py
+++ 기타 자료 구조/1168.py
@@ -3,39 +3,44 @@ import math
 
 N, K = map(int, sys.stdin.readline().split())
 
-max_height = math.ceil(math.log2(2 * N))  # max_height = 4
+max_height = math.ceil(math.log2(2 * N))
 seg_tree = [0] * (2**max_height - 1)
-result = []
+
 
 # initialize a segment tree
 def init(node: int) -> int:
     if node >= 2**(max_height - 1) - 1:
         if node < (2**(max_height - 1) - 1) + N:
-            seg_tree[node] = 1
+            seg_tree[node] += 1
         return seg_tree[node]
 
     seg_tree[node] = init(2 * node + 1) + init(2 * node + 2)
     return seg_tree[node]
 
 
-def update(node: int, idx: int, start: int, end: int):
+# delete and return the i-th element among remaining elements
+def query(node: int, i: int, start: int, end: int) -> int:
     seg_tree[node] -= 1
     if start == end:
-        result.append(start)
-        seg_tree[node] = 0
-        return
+        return start
 
     mid = (start + end) // 2
-    if idx <= mid:
-        update(2 * node + 1, idx, start, mid)
+    if i <= seg_tree[2 * node + 1]:
+        return query(2 * node + 1, i, start, mid)
     else:
-        update(2 * node + 2, idx, mid + 1, end)
+        return query(2 * node + 2, i - seg_tree[2 * node + 1], mid + 1, end)
 
+
+result = []
 
 init(0)
-i = 0
-for _ in range(5):
-    i = (i + K) % N
-    update(0, i, 1, 2**(max_height - 1))  # update(0, 3, 1, 8)
-print(seg_tree)
-print(result)
+idx = 1
+for n in range(N):
+    idx += (K - 1)
+    if idx % (N - n):
+        idx %= (N - n)
+    else:
+        idx = (N - n)
+    num = query(node=0, i=idx, start=1, end=2**(max_height - 1))
+    result.append(str(num))
+print('<' + ', '.join(result) + '>')
