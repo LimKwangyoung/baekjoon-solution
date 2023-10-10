ac96499fdf8efe61b923c1141d1b249409dc67cb
kokoz22 <kokoz22@naver.com>
2023-10-10 22:40:00 +0900

    그래프

--- 그래프/11725.py
+++ 그래프/11725.py
@@ -1,13 +1,28 @@
 import sys
 import collections
 
+
+def bfs() -> list:
+    result = [None] * (N + 1)
+
+    visited = [False, True] + [False] * (N - 1)
+    que = collections.deque([1])
+    while que:
+        node = que.popleft()
+        for i in tree[node]:
+            if not visited[i]:
+                result[i] = str(node)
+                visited[i] = True
+                que.append(i)
+    return result
+
+
 N = int(sys.stdin.readline())
 
 tree = collections.defaultdict(list)
 for _ in range(N - 1):
     v1, v2 = map(int, sys.stdin.readline().split())
-    if v1 == 1:
-        tree[1].append(v2)
-    elif v2 == 1:
-        tree[1].append(v1)
-    elif 
+    tree[v1].append(v2)
+    tree[v2].append(v1)
+
+print('\n'.join(bfs()[2:]))
