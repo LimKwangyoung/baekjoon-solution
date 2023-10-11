26d10813103f81684b20dd79df99c8d7308ca0db
kokoz22 <kokoz22@naver.com>
2023-10-11 16:40:35 +0900

    그래프

--- 그래프/1991.py
+++ 그래프/1991.py
@@ -1,7 +1,7 @@
 import sys
-import collections
 
 
+# Solution 1(Recursion)
 def preorder(node: str) -> None:
     if node != '.':
         print(node, end='')
@@ -35,3 +35,65 @@ print()
 inorder('A')
 print()
 postorder('A')
+
+
+# # Solution 2(Stack)
+# def preorder() -> list[str]:
+#     result = []
+#     stack = ['A']
+#     while stack:
+#         node = stack.pop()
+#         if node != '.':
+#             result.append(str(node))
+#             stack.append(tree[node][1])
+#             stack.append(tree[node][0])
+#     return result
+#
+#
+# def inorder() -> list[str]:
+#     result = []
+#     stack = ['A']
+#     while stack:
+#         node = stack.pop()
+#         if node != '.':
+#             while node != '.':
+#                 stack.append(node)
+#                 node = tree[node][0]
+#
+#         if not stack:
+#             break
+#         node = stack.pop()
+#         result.append(str(node))
+#         stack.append(tree[node][1])
+#     return result
+#
+#
+# def postorder() -> list[str]:
+#     result = []
+#     stack = ['A']
+#     while stack:
+#         node = stack.pop()
+#         if node != '.':
+#             while node != '.':
+#                 stack.append(node)
+#                 node = tree[node][0]
+#
+#         node = stack.pop()
+#         if node != tree[stack[-1]][1]:
+#             result.append(str(node))
+#             stack.append(tree[stack[-1]][1])
+#         else:
+#             stack.append(node)
+#             stack.append(tree[node][1])
+#
+#
+# N = int(sys.stdin.readline())
+#
+# tree = {}
+# for _ in range(N):
+#     parent, left, right = sys.stdin.readline().split()
+#     tree[parent] = [left, right]
+
+# print(''.join(preorder()))
+# print(''.join(inorder()))
+# print(postorder())
