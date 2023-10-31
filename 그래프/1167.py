c1385ca6ef97ce71b233267f66e8fd98bdcb681e
kokoz22 <kokoz22@naver.com>
2023-11-01 00:35:16 +0900

    그래프

--- 그래프/1167.py
+++ 그래프/1167.py
@@ -1,6 +1,28 @@
 import sys
+import collections
+
+
+def bfs(start: int) -> int:
+    result = [0] * (V + 1)
+    que = collections.deque([start])
+    while que:
+        row = que.popleft()
+        for vertex, distance in tree[row]:
+            if vertex != start and not result[vertex]:
+                result[vertex] = result[row] + distance
+                que.append(vertex)
+    return max(result)
+
 
 V = int(sys.stdin.readline())
+
+tree = collections.defaultdict(list)
+leaf = []
 for _ in range(V):
     lst = list(map(int, sys.stdin.readline().split()))[:-1]
-    print(lst)
+    for i in range(1, len(lst), 2):
+        tree[lst[0]].append([lst[i], lst[i + 1]])
+    if i == 1:
+        leaf.append(lst[0])
+
+
