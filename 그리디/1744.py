d5530ee7920d52e9652d66bf5ca978468cac6727
kokoz22 <kokoz22@naver.com>
2024-01-23 22:52:30 +0900

    그리디

--- 그리디/1744.py
+++ 그리디/1744.py
@@ -1,19 +1,65 @@
 import sys
 
+# Solution 1
 N = int(sys.stdin.readline())
 nums = [int(sys.stdin.readline()) for _ in range(N)]
 
 nums.sort()
 
-i = 0
 result = 0
-while i + 1 < N:
-    if nums[i] == 0 or nums[i] == 1 or nums[i] * nums[i + 1] < 0:
-        result += nums[i]
-        i += 1
-    elif nums[i + 1] == 0 or nums[i] * nums[i + 1] > 0:
-        result += nums[i] * nums[i + 1]
-        i += 2
-if i != N:
-    result += nums[i]
+left, right = 0, N - 1
+
+# negative
+while left < N and nums[left] < 0:
+    if left + 1 >= N or nums[left + 1] > 0:
+        result += nums[left]
+        left += 1
+    else:
+        result += nums[left] * nums[left + 1]
+        left += 2
+
+# positive
+while right >= left and nums[right] > 0:
+    if right - 1 < left or nums[right] == 1 or nums[right - 1] == 1:
+        result += nums[right]
+        right -= 1
+    else:
+        result += nums[right] * nums[right - 1]
+        right -= 2
+
+print(result)
+
+# Solution 2
+N = int(sys.stdin.readline())
+
+result = 0
+positive, negative = [], []
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num > 1:
+        positive.append(num)
+    elif num == 1:
+        result += num
+    else:
+        negative.append(num)
+
+positive.sort(reverse=True)
+negative.sort()
+
+if len(positive) % 2:
+    for i in range(0, len(positive) - 1, 2):
+        result += positive[i] * positive[i + 1]
+    result += positive[-1]
+else:
+    for i in range(0, len(positive), 2):
+        result += positive[i] * positive[i + 1]
+
+if len(negative) % 2:
+    for i in range(0, len(negative) - 1, 2):
+        result += negative[i] * negative[i + 1]
+    result += negative[-1]
+else:
+    for i in range(0, len(negative), 2):
+        result += negative[i] * negative[i + 1]
+
 print(result)
