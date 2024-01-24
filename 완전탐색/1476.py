7fdab1658809cf1cf93e1471ddcbb5560362cdd1
kokoz22 <kokoz22@naver.com>
2024-01-24 23:55:24 +0900

    완전탐색

--- 완전탐색/1476.py
+++ 완전탐색/1476.py
@@ -2,26 +2,15 @@ import sys
 
 E, S, M = map(int, sys.stdin.readline().split())
 
-if (E, S, M) == (1, 1, 1):
-    print(1)
-else:
-    e, s, m = E + 1, S + 1, M + 1
-    year = 2
+E -= 1
+S -= 1
+M -= 1
 
-    while (e, s, m) != (E, S, M):
-        if e == 15:
-            e = 1
-        else:
-            e += 1
-
-        if s == 28:
-            s = 1
-        else:
-            s += 1
-
-        if m == 19:
-            m = 1
-        else:
-            m += 1
-        year += 1
-    print(year)
+e, s, m = 0, 0, 0
+year = 1
+while (e, s, m) != (E, S, M):
+    e = (e + 1) % 15
+    s = (s + 1) % 28
+    m = (m + 1) % 19
+    year += 1
+print(year)
