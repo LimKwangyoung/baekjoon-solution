d200e676107b01278f7c5298642297ab7671f0ad
kokoz22 <kokoz22@naver.com>
2024-02-07 01:17:24 +0900

    dp

---
+++ dp/3067.py
@@ -0,0 +1,33 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    M = int(sys.stdin.readline())
+
+    dp = [0] * (M + 1)
+    for i in coins:
+        dp[i] = 1
+
+    for i in range(1, M + 1):
+        if dp[i]:
+            for j in coins:
+                if i + j < M + 1:
+                    dp[i + j] += dp[i]
+    print(dp[M])
+
+
+"""
+3
+2
+1 2
+1000
+3
+1 5 10
+100
+2
+5 7
+22
+"""
