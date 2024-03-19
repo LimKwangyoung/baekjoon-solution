117094e7cff1222b9d10a25f32971dc79d7a9fe5
LimKwangyoung <kokoz22@naver.com>
2024-03-19 09:52:00 +0900

    queue

--- queue/7662.py
+++ queue/7662.py
@@ -7,6 +7,8 @@ T = int(sys.stdin.readline())
 for _ in range(T):
     max_heap, min_heap = [], []
     nums = collections.defaultdict(int)
+
+    cnt = 0
     for _ in range(int(sys.stdin.readline())):
         operator, mode = sys.stdin.readline().split()
         mode = int(mode)
@@ -14,22 +16,21 @@ for _ in range(T):
             heapq.heappush(min_heap, mode)
             heapq.heappush(max_heap, -mode)
             nums[mode] += 1
-        elif nums:
+            cnt += 1
+        elif cnt:
             if mode == 1:  # maximum
-                num = -heapq.heappop(max_heap)
-                nums[num] -= 1
-                if not nums[num]:
-                    del nums[num]
+                while True:
+                    num = -heapq.heappop(max_heap)
+                    if nums[num]:
+                        nums[num] -= 1
+                        break
             else:  # minimum
-                num = heapq.heappop(min_heap)
-                nums[num] -= 1
-                if not nums[num]:
-                    del nums[num]
-        else:
-            max_heap, min_heap = [], []
+                while True:
+                    num = heapq.heappop(min_heap)
+                    if nums[num]:
+                        nums[num] -= 1
+                        break
+            cnt -= 1
 
-        # print('MAX', max_heap, end='\t')
-        # print('MIN', min_heap, end='\t')
-        # print(nums)
-
+    nums = [num for num in nums if nums[num]]
     print('EMPTY' if not nums else f'{max(nums)} {min(nums)}')
