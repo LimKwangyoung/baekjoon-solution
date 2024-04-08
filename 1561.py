f50f39ec44b51f172aea9abf1aad858ed833ddbf
LIMKWANGYOUNG <kokoz22@naver.com>
2024-04-08 17:58:21 +0900

    1561

--- 1561.py
+++ 1561.py
@@ -1,5 +1,4 @@
 import sys
-import heapq
 
 N, M = map(int, sys.stdin.readline().split())
 times = list(map(int, sys.stdin.readline().split()))
@@ -7,15 +6,39 @@ times = list(map(int, sys.stdin.readline().split()))
 if N <= M:
     print(N)
 else:
-    rides = [(time, i) for i, time in enumerate(times)]
-    heapq.heapify(rides)
-    for i in range(M, N):
-        min_time, min_idx = heapq.heappop(rides)
-        heapq.heappush(rides, (min_time + times[min_idx], min_idx))
-        # min_idx, min_time = 0, float('inf')
-        # for idx, time in enumerate(rides):
-        #     if time < min_time:
-        #         min_idx = idx
-        #         min_time = time
-        # rides[min_idx] += times[min_idx]
-    print(min_idx + 1)
+    left, right = 1, N * M
+    N -= M
+    while left <= right:
+        mid = (left + right) // 2
+        num = sum(mid // n for n in times)
+
+        if num >= N:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    left -= 1
+    total = 0
+    for i in range(M):
+        total += (left // times[i])
+        times[i] = left % times[i]
+    idx = 0
+    while N - total:
+        if times[idx] <= 1:
+            N -= 1
+        idx += 1
+    print(idx + 1)
+
+    # cnt = M
+    # result = 0
+    # rides = times[:]
+    # print(f'{result}초: {rides} /// {cnt}명')
+    # while N - M:
+    #     for i in range(len(rides)):
+    #         rides[i] -= 1
+    #         if rides[i] == 0:
+    #             rides[i] = times[i]
+    #             N -= 1
+    #             cnt += 1
+    #     result += 1
+    #     print(f'{result}초: {rides} /// {cnt}명')
