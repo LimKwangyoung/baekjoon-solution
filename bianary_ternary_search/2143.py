aea8edf97248c71bdc2b6b2438725ffba8d1cf3d
kokoz22 <kokoz22@naver.com>
2024-04-14 23:42:45 +0900

    2143;

--- bianary_ternary_search/2143.py
+++ bianary_ternary_search/2143.py
@@ -80,7 +80,28 @@ A_lst = comb_sum(A, n)
 B_lst = sorted(comb_sum(B, m))
 
 result = 0
+# print(f'B_lst: {B_lst}')
 for a in A_lst:
-    target =
+    target = T - a
 
     left, right = 0, len(B_lst)
+    while left <= right:
+        mid = (left + right) // 2
+        if B_lst[mid] >= target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    final_left = left
+
+    left, right = 0, len(B_lst)
+    while left <= right:
+        mid = (left + right) // 2
+        if B_lst[mid] <= target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    final_right = left
+
+    result += (final_right - final_left)
+    # print(f'target: {target}, LEFT: {final_left}, RIGHT: {final_right}')
+print(result)
