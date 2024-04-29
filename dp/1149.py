a5908e23c4ceb74bfa47f8d79eb3e9478741948b
LimKwangyoung <kokoz22@naver.com>
2024-04-29 13:43:04 +0900

    dp

--- dp/1149.py
+++ dp/1149.py
@@ -3,8 +3,13 @@ import sys
 N = int(sys.stdin.readline())
 costs = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-for i in range(1, N):
-    costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
-    costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
-    costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
-print(min(costs[-1]))
+result = float('inf')
+for start in range(3):
+    dp = [[float('inf')] * 3 for _ in range(N)]
+    dp[0][start] = costs[0][start]
+    for i in range(1, N):
+        dp[i][0] = costs[i][0] + min(dp[i - 1][1], dp[i - 1][2])
+        dp[i][1] = costs[i][1] + min(dp[i - 1][0], dp[i - 1][2])
+        dp[i][2] = costs[i][2] + min(dp[i - 1][0], dp[i - 1][1])
+    result = min(result, min(dp[-1]))
+print(result)
